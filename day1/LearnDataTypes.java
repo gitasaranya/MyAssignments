54af5f0f6c08ecc9cfaac3cf78d5cc917aa81c49
gitasaranya <154457821+gitasaranya@users.noreply.github.com>
2023-12-20 23:07:28 +0530

    day 1

---
+++ day1/LearnDataTypes.java
@@ -0,0 +1,32 @@
+package week1.day1;
+
+public class LearnDataTypes {
+
+	public static void main(String[] args) {
+		//Syntax to store value
+		//DataType variable to store values
+		//value ends with f or F for float, else takes as double
+		// value ends with l or L for long, else takes as 
+		
+		int age = 34;
+		byte amount = 127;
+		float piValue = 3.1418f;
+		long creditCardNumber = 1234567891234567l;
+		boolean isEmployed = false;
+		String name = "Gita Saranya";
+		char initial = 'M';
+		char ascii64 = 64;
+		char ascii65 = 65;
+		System.out.println("Age is :"+age);
+		System.out.println("amount is :"+amount);
+		System.out.println("piValue is :"+piValue);
+		System.out.println("creditCardNumber is :"+creditCardNumber);
+		System.out.println("Employment status is :"+isEmployed);
+		System.out.println("name is :"+name);
+		System.out.println("initial is :"+initial);
+		System.out.println("ascii64 is :"+ascii64);
+		System.out.println("ascii65  is :"+ascii65);
+		
+		
+	}
+}
