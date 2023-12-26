c6b0f23f48b3128c43ae8cd34f9bb7aca933acf1
gitasaranya <154457821+gitasaranya@users.noreply.github.com>
2023-12-26 22:11:23 +0530

    week 2 day 1

---
+++ day1/TestClassForSelenium.java
@@ -0,0 +1,14 @@
+package week2.day1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestClassForSelenium {
+	
+	public static void main(String[] args) {
+		
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		
+		}
+
+}
